0d0aadd7e949654b2dd513eda0eac8e09d221e38
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2025-09-05 10:14:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -283,7 +283,7 @@ resource "azurerm_key_vault" "fwkv" {
 
 #log analytics workspace
 resource "azurerm_log_analytics_workspace" "LAW" {
-  name                = "LAW-01"
+  name                = "LAW-${random_pet.name.id}"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
   
@@ -653,7 +653,7 @@ resource "azurerm_firewall" "azfw" {
 }
 #firewall logging
 resource "azurerm_monitor_diagnostic_setting" "fwlogs"{
-  name = "fwlogs"
+  name = "fwlogs-${random_pet.name.id}"
   target_resource_id = azurerm_firewall.azfw.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.LAW.id
   log_analytics_destination_type = "Dedicated"
@@ -856,3 +856,4 @@ resource "azurerm_resource_group_template_deployment" "spokevmcert" {
 TEMPLATE
 depends_on = [ azurerm_resource_group_template_deployment.secret ]
 }
+
